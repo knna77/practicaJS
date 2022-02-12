1024ebf31e0085e5f681b7d2503edb24e59fa136
knna <knna77@gmail.com.com>
2022-02-12 19:45:49 +0100

    test ko

--- examen.js
+++ examen.js
@@ -1,16 +1,16 @@
 const Moment = require("moment");
 class Ordena {
   ordena_noms(llista) {
-  return llista.sort((a, b) => (a.alumne > b.alumne ? 1 : -1));
+  //return llista.sort((a, b) => (a.alumne > b.alumne ? 1 : -1));
   }
   ordena_dates(array) {
-    const sortedArray = array.sort(
+    /*const sortedArray = array.sort(
       (a, b) =>
         new Moment(a.data_naiximent).format("YYYYMMDD") -
         new Moment(b.data_naiximent).format("YYYYMMDD")
     );
-    return sortedArray;
-  }
+    return sortedArray;*/
+   }
 }
 
 module.exports = Ordena;
